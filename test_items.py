e274289ebae2685520fe172b74620f87a0eac0a7
delorian502 <frikazoid502@gmail.com>
2023-06-15 14:12:08 +0300

    update_files

--- test_items.py
+++ test_items.py
@@ -6,5 +6,13 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
-    page_1 = Ec.visibility_of_element_located(browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"))#++++++исправить++++++++++  
-    print(page_1) 
+    time.sleep(30)
+    page_1 = Ec.visibility_of_element_located(browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"))#+++++  
+    
+    button_text = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket").text
+    print(button_text)
+    
+    
+    assert page_1 
+    
+    
