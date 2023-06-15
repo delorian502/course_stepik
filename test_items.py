6d144a69af8a7399eb8f1007fd4128ff866d9c2f
delorian502 <frikazoid502@gmail.com>
2023-06-15 14:23:25 +0300

    changed function name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_guest_should_see_text_on_add_to_cart_button(browser):
     browser.get(link)
     time.sleep(30)
     page_1 = Ec.visibility_of_element_located(browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"))#+++++  
